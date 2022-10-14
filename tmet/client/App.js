2117009c44db0ca99b761c7d46442a1d43b84b6f
sangltv <sangltv@proton.me>
2022-10-14 12:00:32 +0000

    client

---
+++ tmet/client/App.js
@@ -0,0 +1,4 @@
+import React from "react"
+import {H1} from "styled-system-html"
+const App = () => <H1 color="red">Hello React</H1>
+export default App
--- tmet/client/main.js
+++ tmet/client/main.js
@@ -1,1 +1,7 @@
-console.log("Hi Meteor")
+import React from "react"
+import {createRoot} from "react-dom/client"
+
+import App from "./App"
+
+const root = createRoot(document.getElementById("root"))
+root.render( <App /> )
